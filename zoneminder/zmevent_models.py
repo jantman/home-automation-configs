cfe36e6678241ef95630bcd75a6f0b2319eaf0f5
Jason Antman <jason@jasonantman.com>
2018-07-22 08:20:04 -0400

    fix another JSON error

--- zoneminder/zmevent_models.py
+++ zoneminder/zmevent_models.py
@@ -167,13 +167,10 @@ class MonitorZone(object):
         )
 
     @property
-    def as_json(self):
-        d = {
+    def as_dict(self):
+        return {
             x: getattr(self, x) for x in vars(self) if x[0].isupper()
         }
-        return json.dumps(
-            d, sort_keys=True, indent=4, cls=DateSafeJsonEncoder
-        )
 
 
 class ZMEvent(object):
