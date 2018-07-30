86730b35e29437093011155b454423ddde085fe7
Jason Antman <jason@jasonantman.com>
2018-07-30 17:42:47 -0400

    WIP docker-compose

--- testing/config_prep.sh
+++ testing/config_prep.sh
@@ -3,3 +3,7 @@
 rm -Rf hass-config
 cp -a ../homeassistant hass-config
 cp *.yaml hass-config/
+
+rm -Rf appdaemon-config
+cp -a ../appdaemon appdaemon-config
+sed -i 's|http://127.0.0.1:8123|http://hass:8123|' appdaemon-config/appdaemon.yaml
