decbeea344e556117fddfa3cd3433721da11670a
Jason Antman <jason@jasonantman.com>
2018-07-30 19:23:06 -0400

    skeleton of state_setter.py

---
+++ testing/state_setter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import requests
+
+
+INITIAL_STATES = {}
+
+
+class StateSetter(object):
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == "__main__":
+    StateSetter().run()
