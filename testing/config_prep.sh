6a382922cb42c9c7063ddb4e981c2ad5fc128a72
Jason Antman <jason@jasonantman.com>
2018-11-18 12:22:57 -0500

    update testing/config_prep.sh

--- testing/config_prep.sh
+++ testing/config_prep.sh
@@ -3,7 +3,8 @@
 rm -Rf hass-config
 cp -a ../homeassistant hass-config
 cp *.yaml hass-config/
-rm -Rf hass-config/www
+sed -i 's/192.168.0.24/192.168.0.99/g' hass-config/configuration.yaml
+sed -i 's/localhost/192.168.0.102/g' hass-config/configuration.yaml
 
 rm -Rf appdaemon-config
 cp -a ../appdaemon appdaemon-config
