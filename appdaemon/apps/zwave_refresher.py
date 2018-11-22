c4d43383f62a23e94a68c925cd96b89fbc9d1314
Jason Antman <jason@jasonantman.com>
2018-11-21 22:29:46 -0500

    zwave_refresher

---
+++ appdaemon/apps/zwave_refresher.py
@@ -0,0 +1,49 @@
+"""
+AppDaemon app that runs every 5 minutes and refreshes problematic ZWave entities
+"""
+
+import logging
+import datetime
+import appdaemon.plugins.hass.hassapi as hass
+
+from sane_app_logging import SaneLoggingApp
+
+RUN_INTERVAL = 300  # seconds
+
+REFRESH_NODES = {
+    'zwave.ge_45606_2way_dimmer_switch': 19,
+    'zwave.ge_45606_2way_dimmer_switch_2': 20,
+    'zwave.2gig_technologies_ct101_thermostat_iris': 5
+}
+
+#: Default for info-as-debug logging via LogWrapper; can be overridden
+#: at runtime via events. See ``sane_app_logging.py``.
+LOG_DEBUG = False
+
+
+class ZwaveRefresher(hass.Hass, SaneLoggingApp):
+
+    def initialize(self):
+        self._setup_logging(self.__class__.__name__, LOG_DEBUG)
+        self._log.info("Initializing ZwaveRefresher...")
+        self.run_every(
+            self._refresh_zwave,
+            datetime.datetime.now() + datetime.timedelta(seconds=5),
+            RUN_INTERVAL
+        )
+        self._log.info('Done initializing ZwaveRefresher')
+        self.listen_event(self._refresh_zwave, event='ZWAVE_REFRESHER')
+
+    def _refresh_zwave(self, *args, **kwargs):
+        self._log.info('Refreshing problematic Z-Wave entities...')
+        delay = 0
+        for ename, eid in REFRESH_NODES.items():
+            delay += 5
+            self.run_in(self._refresh_node, delay, node_id=eid, node_name=ename)
+        self._log.info('Done. Entities will refresh within %d seconds', delay)
+
+    def _refresh_node(self, kwargs):
+        node_name = kwargs['name']
+        node_id = kwargs['id']
+        self._log.debug('Refreshing ZWave Node %s (%s)', node_id, node_name)
+        self.call_service('zwave.refresh_node', node_id=node_id)
