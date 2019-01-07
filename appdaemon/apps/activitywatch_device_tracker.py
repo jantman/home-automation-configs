da32a801a0d6159a2d48d9cf0fd7c235fbc27efe
Jason Antman <jason@jasonantman.com>
2019-01-06 21:51:28 -0500

    appdaemon - activitywatch_device_tracker - simplify by using heartbeat

--- appdaemon/apps/activitywatch_device_tracker.py
+++ appdaemon/apps/activitywatch_device_tracker.py
@@ -6,7 +6,7 @@ tracker location.
 """
 
 import appdaemon.plugins.hass.hassapi as hass
-from datetime import datetime
+from datetime import datetime, time
 import os
 import requests
 import json
@@ -45,14 +45,11 @@ class ActivityWatchDeviceTracker(hass.Hass, SaneLoggingApp):
         self._base_url = 'http://%s:%d/api/0/' % (AW_HOST, AW_PORT)
         self._bucket_id = 'aw-watcher-hass-device'
         self._create_bucket()
-        c_state = self.get_state(DEVICE_TRACKER_ENTITY)
-        self.start_dt, self.current_state = self._load_state(c_state)
-        if self.current_state != c_state:
-            # else we had a state change while stopped
-            self.send_event(self.current_state, self.start_dt)
-            self.start_dt = datetime.utcnow()
-            self.current_state = c_state
+        self.current_state = self.get_state(DEVICE_TRACKER_ENTITY)
         self.listen_state(self.state_change, DEVICE_TRACKER_ENTITY)
+        self._timer = self.run_minutely(
+            self._timer_callback, time(0, 0, 12)
+        )
         self._log.info('Done initializing ActivityWatchDeviceTracker.')
 
     def _create_bucket(self):
@@ -70,34 +67,17 @@ class ActivityWatchDeviceTracker(hass.Hass, SaneLoggingApp):
         self._log.debug('Response %s: %s', r.status_code, r.text)
         r.raise_for_status()
 
-    def _load_state(self, c_state):
-        start_dt = datetime.utcnow()
-        if not os.path.exists(STATE_PATH):
-            return start_dt, c_state
-        self._log.debug('State path exists (%s); reading', STATE_PATH)
-        with open(STATE_PATH, 'r') as fh:
-            raw = fh.read().strip()
-        self._log.debug('State path content: "%s"', raw)
-        if raw == '':
-            return start_dt, c_state
-        parts = raw.split(':', 1)
-        current_state = parts[1]
-        start_dt = datetime.utcfromtimestamp(int(parts[0]))
-        self._log.debug(
-            'Load from STATE_PATH: current_state=%s start_dt=%s',
-            self.current_state, self.start_dt
-        )
-        return start_dt, current_state
+    def _timer_callback(self, _):
+        self.current_state = self.get_state(DEVICE_TRACKER_ENTITY)
+        self.send_heartbeat(self.current_state)
 
-    def send_event(self, state, start_dt, end_dt=None):
-        if end_dt is None:
-            end_dt = datetime.utcnow()
-        url = self._base_url + 'buckets/%s/events' % self._bucket_id
+    def send_heartbeat(self, state):
+        url = self._base_url + 'buckets/%s/heartbeat?pulsetime=62' \
+                               '' % self._bucket_id
         headers = {"Content-type": "application/json", "charset": "utf-8"}
         data = {
             'id': None,
-            'timestamp': start_dt.isoformat(),
-            'duration': (end_dt - start_dt).total_seconds(),
+            'timestamp': datetime.utcnow().isoformat(),
             'data': {
                 'app': state,
                 'title': state,
@@ -129,6 +109,5 @@ class ActivityWatchDeviceTracker(hass.Hass, SaneLoggingApp):
                 self.current_state
             )
             return
-        self.send_event(self.current_state, self.start_dt)
-        self.start_dt = datetime.utcnow()
+        self.send_heartbeat(new)
         self.current_state = new
