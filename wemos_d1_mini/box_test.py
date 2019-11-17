db51b3e1e7c5dd56f5dd27130d31c28777c3fc01
Jason Antman <jason@jasonantman.com>
2019-11-17 15:34:28 -0500

    WIP debouncing

--- wemos_d1_mini/box_test.py
+++ wemos_d1_mini/box_test.py
@@ -5,21 +5,21 @@ https://wiki.wemos.cc/products:d1:d1_mini#pin
 Connect buttons and LEDs according to ``main.fzz``.
 """
 
-from machine import Pin
+from machine import Pin, Timer
 import micropython
 from time import sleep_ms
 micropython.alloc_emergency_exception_buf(100)
 
 # Pin mappings - board number to GPIO number
-D0 = 16
-D1 = 5
-D2 = 4
-D3 = 0
-D4 = 2
-D5 = 14
-D6 = 12
-D7 = 13
-D8 = 15
+D0 = micropython.const(16)
+D1 = micropython.const(5)
+D2 = micropython.const(4)
+D3 = micropython.const(0)
+D4 = micropython.const(2)
+D5 = micropython.const(14)
+D6 = micropython.const(12)
+D7 = micropython.const(13)
+D8 = micropython.const(15)
 
 BUTTON_MAP = {
     'white': D8,
@@ -48,6 +48,10 @@ class BoxTest:
         for color, pin in BUTTON_MAP.items():
             self.pin_to_button_colors['Pin(%s)' % pin] = color
             self.buttons[color] = Pin(pin, Pin.IN, Pin.PULL_UP)
+        self.debounce_timer = Timer(0)
+        self.timer_running = False
+        print(self.debounce_timer)
+        print(dir(self.debounce_timer))
         print('Finished init')
 
     def set_rgb(self, red, green, blue):
@@ -60,17 +64,39 @@ class BoxTest:
         for pin in self.buttons.values():
             pin.irq(
                 trigger=Pin.IRQ_FALLING,
-                handler=self.on_press
+                handler=self.button_pin_irq_callback
             )
         while True:
             if self.unhandled_event:
                 pass
             sleep_ms(50)
 
-    def on_press(self, pin):
-        micropython.schedule(self.on_press_deferred, pin)
+    def button_pin_irq_callback(self, _):
+        print('button_pin_irq_callback()')
+        if self.timer_running:
+            return
+        self.debounce_timer.init(
+            mode=Timer.ONE_SHOT, period=200,
+            callback=self.button_debounce_timer_irq_callback
+        )
 
-    def on_press_deferred(self, pin):
+    def button_debounce_timer_irq_callback(self, _):
+        print('button_debounce_timer_irq_callback()')
+        self.timer_running = False
+        micropython.schedule(self.on_press_deferred, None)
+
+    def read_buttons(self):
+        res = []
+        for name, pin in self.buttons.items():
+            print(name, pin, pin.value())
+            if not pin.value():
+                res.append(name)
+        return res
+
+    def on_press_deferred(self, _):
+        print('on_press_deferred() called')
+        print(self.read_buttons())
+        return
         color = self.pin_to_button_colors[str(pin)]
         print('on_press_deferred() called for pin: %s (%s)' % (color, pin))
         # print(pin.name)
