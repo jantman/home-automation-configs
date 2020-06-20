09aab6c1c60fbf390ea04af4a16cce6160feb2b7
Jason Antman <jason@jasonantman.com>
2020-06-19 22:21:10 -0400

    currently working

--- wemos_d1_mini/humidor_sht85.py
+++ wemos_d1_mini/humidor_sht85.py
@@ -5,6 +5,7 @@ SHT85 sensor wired according to sht85.md
 """
 
 import micropython
+from micropython import const
 micropython.alloc_emergency_exception_buf(100)
 from machine import Pin, I2C
 import network
@@ -12,9 +13,14 @@ import socket
 from time import sleep, sleep_ms
 from binascii import hexlify
 import time
-from sht85 import SHT31D
+import time
 import json
 from config import SSID, WPA_KEY, HOOK_HOST, HOOK_PORT, HOOK_PATH
+from i2c_device import I2CDevice
+try:
+    import struct
+except ImportError:
+    import ustruct as struct
 
 # Pin mappings - board number to GPIO number
 SDA = micropython.const(4)  # D2
@@ -26,9 +32,106 @@ ENTITIES = {
 }
 
 FRIENDLY_NAMES = {
-    '500291c9b1a3': '500291c9b1a3',
+    '500291c9b1a3': 'Humidor',
 }
 
+# BEGIN Condensed version of sht85.py
+_SHT31_DEFAULT_ADDRESS = const(0x44)
+REP_HIGH = "High"
+FREQUENCY_4 = 4
+_SHT31_PERIODIC_BREAK = const(0x3093)
+_SHT31_SOFTRESET = const(0x30A2)
+_SHT31_READSERIALNBR = const(0x3780)
+
+def _crc(data):
+    crc = 0xFF
+    for byte in data:
+        crc ^= byte
+        for _ in range(8):
+            if crc & 0x80:
+                crc <<= 1
+                crc ^= 0x131
+            else:
+                crc <<= 1
+    return crc
+
+
+def _unpack(data):
+    length = len(data)
+    crc = [None] * (length // 3)
+    word = [None] * (length // 3)
+    for i in range(length // 6):
+        word[i * 2], crc[i * 2], word[(i * 2) + 1], crc[(i * 2) + 1] = struct.unpack(
+            ">HBHB", data[i * 6 : (i * 6) + 6]
+        )
+        if crc[i * 2] == _crc(data[i * 6 : (i * 6) + 2]):
+            length = (i + 1) * 6
+    for i in range(length // 3):
+        if crc[i] != _crc(data[i * 3 : (i * 3) + 2]):
+            raise RuntimeError("CRC mismatch")
+    return word[: length // 3]
+
+
+class SHT85:
+
+    def __init__(self, i2c_bus, address=_SHT31_DEFAULT_ADDRESS):
+        self.i2c_device = I2CDevice(i2c_bus, address)
+        self._repeatability = REP_HIGH
+        self._frequency = FREQUENCY_4
+        self._last_read = 0
+        self._cached_temperature = None
+        self._cached_humidity = None
+        self._reset()
+
+    def _command(self, command):
+        self.i2c_device.write(struct.pack(">H", command))
+
+    def _reset(self):
+        """
+        Soft reset the device
+        The reset command is preceded by a break command as the
+        device will not respond to a soft reset when in 'Periodic' mode.
+        """
+        print("SHT31D._reset() Sending periodic break to device")
+        self._command(_SHT31_PERIODIC_BREAK)
+        time.sleep(0.001)
+        print("SHT31D._reset() Sending soft reset to device")
+        self._command(_SHT31_SOFTRESET)
+        time.sleep(0.0015)
+
+    def _data(self):
+        data = bytearray(6)
+        data[0] = 0xFF
+        self._command(const(0x2400))
+        time.sleep(0.0155)
+        self.i2c_device.readinto(data)
+        word = _unpack(data)
+        length = len(word)
+        temperature = [None] * (length // 2)
+        humidity = [None] * (length // 2)
+        for i in range(length // 2):
+            temperature[i] = -45 + (175 * (word[i * 2] / 65535))
+            humidity[i] = 100 * (word[(i * 2) + 1] / 65535)
+        if (len(temperature) == 1) and (len(humidity) == 1):
+            return temperature[0], humidity[0]
+        return temperature, humidity
+
+    def _read(self):
+        self._cached_temperature, self._cached_humidity = self._data()
+        return self._cached_temperature, self._cached_humidity
+
+    @property
+    def serial_number(self):
+        """Device serial number."""
+        data = bytearray(6)
+        data[0] = 0xFF
+        self._command(_SHT31_READSERIALNBR)
+        time.sleep(0.001)
+        self.i2c_device.readinto(data)
+        word = _unpack(data)
+        return (word[0] << 16) | word[1]
+
+# END Condensed version of sht85.py
 
 class HumidorSender:
 
@@ -53,11 +156,8 @@ class HumidorSender:
             freq=1000
         )
         print("Initializing sensor...")
-        self.sensor = SHT31D(self.i2c)
+        self.sensor = SHT85(self.i2c)
         print("Serial number: %s", self.sensor.serial_number)
-        stat = self.sensor.status
-        print("Status: %s", stat)
-        print("Status hex: %x", stat)
         print("Done initializing.")
 
     def run(self):
