3b854a8f444f07dc5531f0d6c2b8b858105c2425
Jason Antman <jason@jasonantman.com>
2020-11-15 16:51:16 -0500

    auth

--- zoneminder/zmeventnotification_handler.py
+++ zoneminder/zmeventnotification_handler.py
@@ -13,10 +13,17 @@ async def handle():
     uri = "ws://localhost:9000"
     print('Connecting to: %s' % uri)
     async with websockets.connect(uri) as websocket:
+        print('Connected')
+        """
         print('Connected; sending version request')
         await websocket.send('{"event":"control","data":{"type":"version"}}')
         response = await websocket.recv()
         print('Version response: %s' % response)
+        """
+        auth = '{"event":"auth","data":{"user":"u","password":"p"}}'
+        print('Sending auth message: %s', auth)
+        response = await websocket.recv()
+        print('Auth response: %s' % response)
         print('Listening for messages...')
         while True:
             message = await websocket.recv()
