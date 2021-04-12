b10084f9d524e6d8cc8b7d305e5aedbd36831f81
Jason Antman <jason@jasonantman.com>
2021-04-12 17:56:35 -0400

    zmevent_backfiller bug fix

--- zoneminder/zmevent_backfiller.py
+++ zoneminder/zmevent_backfiller.py
@@ -111,9 +111,9 @@ def main():
     args = parse_args(sys.argv[1:])
     # set logging level
     if args.verbose > 1:
-        set_log_debug()
+        set_log_debug(logger)
     else:
-        set_log_info()
+        set_log_info(logger)
     ZmEventBackfiller().run(args.FIRST_EVENT_ID, sleep_time=args.sleep)
 
 
