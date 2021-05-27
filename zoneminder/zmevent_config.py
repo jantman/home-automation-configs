ed86d3e58c98dde4d6b377b865758bdedf4d35c7
Jason Antman <jason@jasonantman.com>
2021-05-27 07:38:16 -0400

    guarddog - bump up retry_start_id to deal with broken event

--- zoneminder/zmevent_config.py
+++ zoneminder/zmevent_config.py
@@ -40,7 +40,7 @@ STATSD_HOST = '192.168.0.24'
 STATSD_PORT = 8125
 
 #: Minimum event ID to backfill or retry
-RETRY_START_ID = 626707
+RETRY_START_ID = 670988
 
 #: Configuration populated from environment variables; see
 #: :py:func:`~.populate_secrets`
