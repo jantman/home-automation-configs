e799940da78250f1d6b1dc2a0b9432aaed0f1380
Jason Antman <jason@jasonantman.com>
2021-12-01 12:46:51 -0500

    update ignored object

--- zoneminder/zmevent_config.py
+++ zoneminder/zmevent_config.py
@@ -121,7 +121,7 @@ def is_garage_zipper_as_person(i, label, x, y, w, h, zones, score):
     """
     if (
         515 < x < 542 and
-        232 < y < 261 and
+        230 < y < 261 and
         140 < w < 300 and
         468 < h < 540 and
         label == 'person'
