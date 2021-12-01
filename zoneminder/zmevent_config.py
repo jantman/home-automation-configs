0e4b008cfb462f693386f562c6d8412eae7e77af
Jason Antman <jason@jasonantman.com>
2021-12-01 12:49:30 -0500

    tweak last commit

--- zoneminder/zmevent_config.py
+++ zoneminder/zmevent_config.py
@@ -122,7 +122,7 @@ def is_garage_zipper_as_person(i, label, x, y, w, h, zones, score):
     if (
         515 < x < 542 and
         230 < y < 261 and
-        140 < w < 300 and
+        130 < w < 300 and
         468 < h < 540 and
         label == 'person'
     ):
