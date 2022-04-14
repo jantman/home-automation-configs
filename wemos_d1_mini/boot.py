b5a57e407d66c0aa284b16ef7e914c8c3e4a8270
Jason Antman <jason@jasonantman.com>
2022-04-14 17:23:22 -0400

    wemos boot.py - remove unused import

--- wemos_d1_mini/boot.py
+++ wemos_d1_mini/boot.py
@@ -1,7 +1,7 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 #import esp
 #esp.osdebug(None)
-import uos, machine
+#import uos, machine
 #uos.dupterm(None, 1) # disable REPL on UART(0)
 import gc
 import webrepl
