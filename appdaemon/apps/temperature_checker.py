80cd6e22281606c22723bd5032494cd1fac80f47
Jason Antman <jason@jasonantman.com>
2022-07-07 10:19:49 -0400

    remove octoprint

--- appdaemon/apps/temperature_checker.py
+++ appdaemon/apps/temperature_checker.py
@@ -42,10 +42,6 @@ IGNORE_IDS = [
     'sensor.humidor_temp',
     'sensor.500291c9b1a3_temp',
     'sensor.f4cfa2d0de01_temp',
-    'sensor.octoprint_actual_bed_temp',
-    'sensor.octoprint_target_bed_temp',
-    'sensor.octoprint_actual_tool0_temp',
-    'sensor.octoprint_target_tool0_temp',
     'sensor.officelightswitch_temperature_2',
 ]
 
