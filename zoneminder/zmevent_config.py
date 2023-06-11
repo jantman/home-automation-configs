a03a311f546940aaace407489e6dd93ae6195977
Jason Antman <jason@jasonantman.com>
2023-06-11 12:13:20 -0400

    bump retry start ID

--- zoneminder/zmevent_config.py
+++ zoneminder/zmevent_config.py
@@ -58,7 +58,7 @@ if node() == 'telescreen':
     CONFIG['BASE_URL'] = 'http://redirect.jasonantman.com/telescreen/'
     ZM_HOSTNAME = 'telescreen'
     CONFIG['HASS_API_URL'] = 'http://192.168.0.102:8123/api'
-    RETRY_START_ID = 1729301
+    RETRY_START_ID = 2570621
 
 
 def populate_secrets():
