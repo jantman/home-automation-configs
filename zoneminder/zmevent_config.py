5d430fc24a4c78695f119a93aafb0755a6d57f5b
Jason Antman <jason@jasonantman.com>
2023-06-25 11:18:55 -0400

    bump retry start ID

--- zoneminder/zmevent_config.py
+++ zoneminder/zmevent_config.py
@@ -40,7 +40,7 @@ STATSD_HOST = '192.168.0.24'
 STATSD_PORT = 8125
 
 #: Minimum event ID to backfill or retry
-RETRY_START_ID = 1853572
+RETRY_START_ID = 1878077
 
 #: Configuration populated from environment variables; see
 #: :py:func:`~.populate_secrets`
